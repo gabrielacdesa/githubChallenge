9142e5eda6c911860dd919e31508c5b98f8356a1
gabrielacdesa <carlotti.gabriela0@gmail.com>
2019-09-06 12:31:12 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+//Importações
+import { GithubService } from './services/github.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
+
+  //Adição das importações realizadas (HttpClientModule e ReactiveFormsModule)
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  //Adição do GithubService dentro do providers
+  providers: [GithubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
